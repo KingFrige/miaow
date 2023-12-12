09048385f521c4e8eeea665f8dac69e0f14fef9e
korben.dong <ghostyuan@outlook.com>
2023-12-12 08:33:36 +0800

    add benchmarks.tar.gz

---
+++ src/verilog/tb/run_benchmarks.sh
@@ -0,0 +1,22 @@
+#/bin/bash -f
+
+./run.pl -f -r 0 -dumpfsdb -t BinarySearch         -o BinarySearch
+./run.pl -f -r 0 -dumpfsdb -t BinomialOption       -o BinomialOption
+./run.pl -f -r 0 -dumpfsdb -t BitonicSort          -o BitonicSort
+./run.pl -f -r 0 -dumpfsdb -t BlackScholes         -o BlackScholes
+./run.pl -f -r 0 -dumpfsdb -t BoxFilter            -o BoxFilter
+./run.pl -f -r 0 -dumpfsdb -t DCT                  -o DCT
+./run.pl -f -r 0 -dumpfsdb -t DwtHaar1D            -o DwtHaar1D
+./run.pl -f -r 0 -dumpfsdb -t EigenValue           -o EigenValue
+./run.pl -f -r 0 -dumpfsdb -t FastWalshTransform   -o FastWalshTransform
+./run.pl -f -r 0 -dumpfsdb -t FloydWarshall        -o FloydWarshall
+./run.pl -f -r 0 -dumpfsdb -t Histogram            -o Histogram
+./run.pl -f -r 0 -dumpfsdb -t MatrixMultiplication -o MatrixMultiplication
+./run.pl -f -r 0 -dumpfsdb -t MatrixTranspose      -o MatrixTranspose
+./run.pl -f -r 0 -dumpfsdb -t PrefixSum            -o PrefixSum
+./run.pl -f -r 0 -dumpfsdb -t RadixSort            -o RadixSort
+./run.pl -f -r 0 -dumpfsdb -t RecursiveGaussian    -o RecursiveGaussian
+./run.pl -f -r 0 -dumpfsdb -t Reduction            -o Reduction
+./run.pl -f -r 0 -dumpfsdb -t ScanLargeArrays      -o ScanLargeArrays
+./run.pl -f -r 0 -dumpfsdb -t SobelFilter          -o SobelFilter
+./run.pl -f -r 0 -dumpfsdb -t URNG                 -o URNG
